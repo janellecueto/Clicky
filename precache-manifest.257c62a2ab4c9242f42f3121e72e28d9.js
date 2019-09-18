8a766508191f3f872ce7888b6150cf066c06f097
Janelle Cueto <janellecueto@gmail.com>
2019-09-17 19:22:43 -0500

    Updates

---
+++ precache-manifest.257c62a2ab4c9242f42f3121e72e28d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943f8228b0327fdd2c698870963c132b",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "a51f7a69db61a516767e",
+    "url": "/Clicky/static/js/2.9fdd8471.chunk.js"
+  },
+  {
+    "revision": "7de742c692276f44abef",
+    "url": "/Clicky/static/js/main.2d59d6e3.chunk.js"
+  },
+  {
+    "revision": "d95753e291f80f1b1169",
+    "url": "/Clicky/static/js/runtime~main.f550e08a.js"
+  }
+]);
